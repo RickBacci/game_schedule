4addebba3fdfa187c26e4ba23d79888cc87feed4
Rick Bacci <rickbacci@gmail.com>
2016-07-14 11:27:41 -0400

    create User model
    - add current_user helper method

---
+++ db/migrate/20160713215710_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :token
+      t.string :refresh_token
+      t.datetime :expires_at
+      t.string  :role
+
+      t.timestamps
+    end
+  end
+end
