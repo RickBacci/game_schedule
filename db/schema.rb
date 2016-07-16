3af9bcd524a6949883bbd32ef56117b47dba611f
Rick Bacci <rickbacci@gmail.com>
2016-07-16 18:14:27 -0400

    remove unused role column in User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715211111) do
+ActiveRecord::Schema.define(version: 20160716221258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20160715211111) do
     t.string   "token"
     t.string   "refresh_token"
     t.datetime "expires_at"
-    t.string   "role"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
